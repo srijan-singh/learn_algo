377bcb595f40ae660d700ec9c904fc91eb682e1d
Srijan Singh <srijanssj5goku@gmail.com>
2022-02-12 22:22:09 +0530

    Codes

--- Program/practical_12.cpp
+++ Program/practical_12.cpp
@@ -1,4 +1,4 @@
-// Factorial with Memo
+// Fibonacci with Memo
 
 #include <iostream>
 #include <unordered_map>
@@ -7,27 +7,29 @@ using std::unordered_map;
 
 typedef unordered_map<int, long> Memo;
 
-long long factorail(int value, Memo memo = {})
+long long fib(int value, Memo memo = {})
 {
-    if(memo[value] != NULL)
+    if(memo.count(value) != 0)
     {
         return memo[value];
     }
 
     //Base Case/Termination Case
-    // 1! = 1 and 0! = 1
+    
+    // fib(1) = 1 and fib(0) = 1
     if(value <= 1)
     {
         return 1;
     }
-    // n! = n * (n-1)!
 
-    memo[value] = value * factorail(value-1);
+    // fib(n) = fib(n-1) + fib(n-2)
+    memo[value] = fib(value-1, memo) + fib(value-2, memo);
 
     return memo[value]; 
 }
 
 int main()
 {
-    std::cout<<factorail(14);
+    std::cout<<fib(36);
+    return 0;
 }
